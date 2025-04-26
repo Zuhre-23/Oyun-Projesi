d1bfffcfbec65333fdf50f849bfcdb2b26a8ce52
Zühre Özen <ozenzuhre63@gmail.com>
2025-04-26 20:47:55 +0300

    Oyun ekranı oluşturuldu

--- game.py
+++ game.py
@@ -2,5 +2,11 @@ import pygame
 
 pygame.init()
 
+window=pygame.display.set_mode((800,600))
+flag=True
 
+while flag:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            flag = False
 
