f625350d12be9df99779a603755dbf2ad61158e9
Zühre Özen <ozenzuhre63@gmail.com>
2025-05-06 15:11:52 +0300

    Level sınıfına sahne değişimi yapmak için evren değiştirme fonksiyonu eklendi, game sınıfında 2 farklı evren çizdirildi, yatay hareketli platform eklendi ,çizdirildi ve hareket etmesi için update fonksiyonu yazıldı.

--- game.py
+++ game.py
@@ -1,5 +1,6 @@
 import pygame
 from level import Level
+from player import Player
 GENİSLİK , YUKSEKLİK = 1000,700
 FPS=60
 
@@ -15,6 +16,7 @@ class Game:
         self.arkaplan_2 = pygame.image.load("Background_2.png")
         self.arkaplan_2_boyut = pygame.transform.scale(self.arkaplan_2,size=(GENİSLİK,YUKSEKLİK))
         self.level=Level()
+        self.player = Player(50, 600, self.level)
 
     def run(self):
         oyun_acik = True
@@ -23,12 +25,24 @@ class Game:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     oyun_acik = False
-
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_SPACE:
+                        self.level.evren_degistir()
+                    elif event.key == pygame.K_ESCAPE:
+                        oyun_acik=False
+                    
             self.pencere.blit(self.arkaplan_boyut,(0,0))
             self.level.update_door_animation()
+
+            self.level.update(self.player)
 
             self.draw()
     
     def draw(self):
+        if self.level.active_evren == 1:
+            self.pencere.blit(self.arkaplan_boyut, (0, 0))
+        else:
+            self.pencere.blit(self.arkaplan_2_boyut, (0, 0))
+        
         self.level.draw(self.pencere)
         pygame.display.update()
--- level.py
+++ level.py
@@ -11,12 +11,24 @@ class Level:
             pygame.Rect(420,300,100,100),
         ]
 
-        self.platform_resmi = pygame.image.load("Platform.PNG")
+        self.platform_1 = pygame.image.load("Platform.PNG")
+        self.active_evren = 1
+        self.yatay_hareketli_platform =pygame.Rect(500, 550, 120, 20)
+        self.yatay_hareket_yönü = 1
+        self.hareketli_platform_karakter =False
 
         self.door = pygame.Rect(900,550,150,300)
         self.door_frames = load_images("animations//door","portal1_frame",6)
         self.door_current_frame = 0
         self.door_frame_timer = 0
+        
+
+
+    def evren_degistir(self):
+        if self.active_evren == 1:
+            self.active_evren = 2
+        else :
+            self.active_evren = 1
 
     def update_door_animation(self): #kapı animasyonun sürekli ççalışmasını sağlar
             self.door_frame_timer += 1
@@ -24,12 +36,24 @@ class Level:
                 self.door_current_frame = (self.door_current_frame+1)% len(self.door_frames)
                 self.door_frame_timer = 0
 
+    def update(self,player):
+        self.yatay_hareketli_platform.x += 2 * self.yatay_hareket_yönü
+        if self.yatay_hareketli_platform.left <= 100 or self.yatay_hareketli_platform.right >= 700:
+            self.yatay_hareket_yönü *= -1
 
+        if self.hareketli_platform_karakter:
+           player.rect.x += 2 * self.yatay_hareket_yönü 
 
     def draw (self, surface ):
         for rect in self.platforms:
-            surface.blit(self.platform_resmi, rect.topleft)
+            surface.blit(self.platform_1, rect.topleft)
 
+        platform_resmi=self.platform_1
+        platform_resmi_boyut=pygame.transform.scale(platform_resmi, (self.yatay_hareketli_platform.width, self.yatay_hareketli_platform.height))
+        surface.blit(platform_resmi_boyut, self.yatay_hareketli_platform.topleft)
+        
         if self.door_frames:
             current_frame = self.door_frames[self.door_current_frame]
             surface.blit(current_frame,self.door.topleft)
+
+        
